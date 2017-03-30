0162a09c0337c7631bba1e0a6bd36d778414de64
Anastasia <anastasia.gorbatenko@optimalbi.com>
2017-03-30 16:05:34 +1300

    dev

--- tests/dv_scheduler.sql
+++ tests/dv_scheduler.sql
@@ -113,7 +113,7 @@ CREATE TABLE dv_schedule_task_queue_history
 
 
 -- procedure to generate executable statement for load of any type
-
+/*
 CREATE OR REPLACE FUNCTION fn_get_dv_object_load_script(object_key_in INTEGER, object_type_in VARCHAR(50),
                                                         load_type_in  VARCHAR(30), owner_key_in INTEGER)
   RETURNS TEXT AS
@@ -176,7 +176,70 @@ BEGIN
 END
 $BODY$
 LANGUAGE plpgsql;
+*/
 
+CREATE OR REPLACE FUNCTION fn_get_dv_object_load_script(object_key_in INTEGER, object_type_in VARCHAR(50),
+                                                        load_type_in  VARCHAR(30), owner_key_in INTEGER)
+  RETURNS TEXT AS
+$BODY$
+DECLARE
+  sql_v TEXT;
+BEGIN
+
+  CASE object_type_in
+    WHEN 'business_rule'
+    THEN
+      -- 1. business_rule/ stage table
+      -- if it is stored procedure then different
+      SELECT (CASE WHEN business_rule_type = 'procedure'
+        THEN 'select '
+              ELSE '' END) || business_rule_logic
+      INTO sql_v
+      FROM dv_business_rule
+      WHERE business_rule_key = object_key_in
+            AND is_retired = false
+            AND owner_key = owner_key_in;
+
+    WHEN 'hub'
+    THEN
+      -- 2. hub
+      SELECT DISTINCT
+        'select ore_config.dv_config_dv_load_hub("' || st.stage_table_schema || '","' || st.stage_table_name || '","' ||
+        h.hub_schema || '","' ||
+        h.hub_name || '");'
+      INTO sql_v
+      FROM dv_hub h
+        JOIN dv_hub_key_column hk ON h.hub_key = hk.hub_key
+        JOIN dv_hub_column hc ON hc.hub_key_column_key = hk.hub_key_column_key
+        JOIN dv_stage_table_column sc ON sc.column_key = hc.column_key
+        JOIN dv_stage_table st ON st.stage_table_key = sc.stage_table_key
+      WHERE h.owner_key = owner_key_in AND h.is_retired = false AND st.is_retired = false AND sc.is_retired = false
+            AND st.stage_table_key = object_key_in;
+    WHEN 'satellite'
+    THEN
+      -- 3. satellite
+      SELECT DISTINCT
+        'select ore_config.dv_config_dv_load_satellite("' || st.stage_table_schema || '","' || st.stage_table_name || '","' ||
+        s.satellite_schema || '","' || s.satellite_name || '","' || load_type_in || '");'
+      INTO sql_v
+      FROM dv_satellite s
+        JOIN dv_satellite_column sc ON sc.satellite_key = s.satellite_key
+        JOIN dv_stage_table_column stc ON stc.column_key = sc.column_key
+        JOIN dv_stage_table st ON stc.stage_table_key = st.stage_table_key
+      WHERE s.is_retired = false AND st.is_retired = false AND stc.is_retired = false
+            AND s.owner_key = owner_key_in
+            AND st.stage_table_key = object_key_in;
+  ELSE
+    -- 4. source or anything else -  nothing
+    sql_v:='';
+  END CASE;
+
+
+  RETURN sql_v;
+
+END
+$BODY$
+LANGUAGE plpgsql;
 
 -- list of valid schedule tasks & execution sctipts
 CREATE OR REPLACE VIEW dv_schedule_valid_tasks AS
